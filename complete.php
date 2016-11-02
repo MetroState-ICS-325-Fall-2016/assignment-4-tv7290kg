f6f35f98b5aefb977e46417a277708f32be50837
Unknown <nsjy828@gmail.com>
2016-11-01 19:53:09 -0500

    done

--- complete.php
+++ complete.php
@@ -10,14 +10,21 @@ require 'FormHelper.php';
 $sweets = array('puff' => 'Sesame Seed Puff',
                 'square' => 'Coconut Milk Gelatin Square',
                 'cake' => 'Brown Sugar Cake',
-                'ricemeat' => 'Sweet Rice and Meat');
+                'ricemeat' => 'Sweet Rice and Meat',
+                'icecream' => 'Ice Cream');
 
 $main_dishes = array('cuke' => 'Braised Sea Cucumber',
                      'stomach' => "Sauteed Pig's Stomach",
                      'tripe' => 'Sauteed Tripe with Wine Sauce',
                      'taro' => 'Stewed Pork with Taro',
                      'giblets' => 'Baked Giblets with Salt',
-                     'abalone' => 'Abalone with Marrow and Duck Feet');
+                     'abalone' => 'Abalone with Marrow and Duck Feet',
+                     'Cheesepizza' => 'Cheese Pizza');
+$drink = array('Coke'=> "Coke",
+               'DCoke'=> "Diet Coke",
+               'Spite' => "Sprite",
+               'milk' => 'Milk',
+               'water' => 'Water');
 
 // The main page logic:
 // - If the form is submitted, validate and then process or redisplay
@@ -38,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 function show_form($errors = array()) {
     $defaults = array('delivery' => 'yes',
-                      'size'     => 'medium');
+                      'size'     => 'large');
     // Set up the $form object with proper defaults
     $form = new FormHelper($defaults);
 
@@ -59,13 +66,27 @@ function validate_form( ) {
     if (! strlen($input['name'])) {
         $errors[] = 'Please enter your name.';
     }
+    //email is required
+    if (isset($_POST['email'])) {
+        $input['email'] = trim($_POST['email']);
+
+        if (false===filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL)) {
+            $errors[]="Email is not valid";
+        }
+
+    } else {
+        $input['email'] = '';
+    }
+    if (! strlen($input['email'])) {
+        $errors[] = 'Please enter your email.';
+    }
     // size is required
     if(isset($_POST['size'])) {
         $input['size'] = trim($_POST['size']);
     } else {
         $input['size'] = '';
     }
-    if (! in_array($input['size'], ['small','medium','large'])) {
+    if (! in_array($input['size'], ['small','medium','large','XLarge'])) {
         $errors[] = 'Please select a size.';
     }
     // sweet is required
@@ -74,6 +95,16 @@ function validate_form( ) {
     } else {
         $input['sweet'] = '';
     }
+
+    if (! array_key_exists($input['sweet'], $GLOBALS['sweets'])) {
+        $errors[] = 'Please select a valid sweet item.';
+    }
+    if (isset($_POST['drink'])) {
+        $input['drink'] = $_POST['drink'];
+    } else {
+        $input['drink'] = '';
+    }
+
     if (! array_key_exists($input['sweet'], $GLOBALS['sweets'])) {
         $errors[] = 'Please select a valid sweet item.';
     }
@@ -114,9 +145,12 @@ function validate_form( ) {
 function process_form($input) {
     // look up the full names of the sweet and the main dishes in
     // the $GLOBALS['sweets'] and $GLOBALS['main_dishes'] arrays
+    /*print_r($GLOBALS);*/
     $sweet = $GLOBALS['sweets'][ $input['sweet'] ];
     $main_dish_1 = $GLOBALS['main_dishes'][ $input['main_dish'][0] ];
     $main_dish_2 = $GLOBALS['main_dishes'][ $input['main_dish'][1] ];
+    $drinks = $GLOBALS ['drink'] [ $input ['drink']];
+
     if (isset($input['delivery']) && ($input['delivery'] == 'yes')) {
         $delivery = 'do';
     } else {
@@ -124,9 +158,11 @@ function process_form($input) {
     }
     // build up the text of the order message
     $message=<<<_ORDER_
-Thank you for your order, {$input['name']}.
+ "Thank you for your order, {$input['name']} at {$input['email']}
 You requested the {$input['size']} size of $sweet, $main_dish_1, and $main_dish_2.
+you would like $drinks.
 You $delivery want delivery.\n
+
 _ORDER_;
     if (strlen(trim($input['comments']))) {
         $message .= 'Your comments: '.$input['comments'];
@@ -138,5 +174,7 @@ _ORDER_;
     // print the message, but encode any HTML entities
     // and turn newlines into <br/> tags
     print str_replace('&NewLine;', "<br />\n", htmlentities($message, ENT_HTML5));
+
+
 }
 
